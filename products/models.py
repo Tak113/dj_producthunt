02c89040a0718ff7a96e8c4a6ff269b1a7e39d44
Tak113 <takniques@gmail.com>
2020-10-11 13:01:48 -0700

    initial app release

--- products/models.py
+++ products/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Product(models.Model):
+
+	title = models.CharField(max_length=200)
+	pub_date = models.DateTimeField()
+	body = models.TextField()
+	url = models.URLField(max_length=200)
+	image = models.ImageField(upload_to="img/")
+	icon = models.ImageField(upload_to="img/")
+	votes_total = models.IntegerField(default=1)
+	hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	#change title name in admin page to be title
+	def __str__(self):
+		return self.title
+	
+	def summary(self):
+		return self.body[:200]
+
+	# simplify date output converting to
+	def pub_date_pretty(self):
+			return self.pub_date.strftime('%b %e %Y') #just yr,date,mo
--- products/urls.py
+++ products/urls.py
@@ -13,18 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-from django.contrib import admin
 from django.urls import path, include #added "include" for accounts url setting
-from products import views #added
-from django.conf import settings
-from django.conf.urls.static import static
+from . import views #added
 
 urlpatterns = [
-	path('admin/',admin.site.urls),
-	path('',views.home, name='home'),
-	path('accounts/', include('accounts.urls')),
-	path('products/', include('products.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
+    path('create',views.create,name='create'), #added
+    path('<int:product_id>',views.detail,name='detail'), #added
+    path('<int:product_id>/upvote',views.upvote,name='upvote'), #added
+]
